2196387011b8cbca1a53b9deee7b70327db34c5d
DmitryZzz <yes.zimin@gmail.com>
2022-09-21 21:23:56 +0300

    Changes to be committed: 	modified:   cmd/web/handlers.go 	modified:   cmd/web/routes.go 	modified:   go.mod 	modified:   go.sum 	modified:   ui/html/base.layout.tmpl 	new file:   ui/html/create.page.tmpl 	modified:   ui/html/home.page.tmpl

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,10 +10,6 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -22,15 +18,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.render(w, r, "home.page.tmpl", &templateData{Snippets: s})
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
-
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -47,22 +38,23 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
+}
+
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.page.tmpl", nil)
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
+
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	title := "0 snail"
-	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n - Kobayashi Issa"
-	expires := "7"
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
@@ -70,5 +62,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module github.com/dmitryzzz/snippetbox
 
 go 1.19
 
+require github.com/justinas/alice v1.2.0
+
 require (
-	github.com/justinas/alice v1.2.0
+	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
 	github.com/lib/pq v1.10.7 // indirect
 )
